fe769ad13469c9b6bf3c7d3b9300be1772c31964
gableh <gable.heng@gmail.com>
2015-02-25 23:59:00 +0800

    added add test case

--- TextBuddyTest.java
+++ TextBuddyTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,25 +21,51 @@ public class TextBuddyTest {
 		controller.executeCommand("add c");
 	}
 	@Test
+	public void testAdd() {
+		try {
+	        Method storageAdd = Storage.class.getDeclaredMethod("add",String[].class);
+			Storage storage = new Storage("textbuddy.txt");
+			String testString = "add abc";
+			String[] splitTestString = testString.split(" ");
+			Object[] argObjects = {splitTestString}; 
+	        storageAdd.setAccessible(true);
+	        try {
+	            storageAdd.invoke(storage, argObjects);
+            } catch (IllegalAccessException e) {
+	            e.printStackTrace();
+            } catch (IllegalArgumentException e) {
+	            e.printStackTrace();
+            } catch (InvocationTargetException e) {
+	            e.printStackTrace();
+            }
+			Storage storage1 = new Storage("textbuddy.txt");
+			assertEquals("test add","abc",storage1.getContents().get(storage1.getContents().size()-1));
+        } catch (NoSuchMethodException e) {
+	        e.printStackTrace();
+        } catch (SecurityException e) {
+	        e.printStackTrace();
+        }
+	}
+	@Test
 	public void testController() {
 		assertEquals("test constructor","textbuddy.txt",controller.getFileName());
 		assertEquals("test is running",true,controller.getIsRunning());
 		
 	}
 	@Test
-	public void testStorage(){
+	public void testStorage() {
 		Storage storage = new Storage("textbuddy.txt");
 		assertEquals("test storage constructor",true,storage.getIsSuccessful());
 	}
 	@Test
-	public void testSort(){
+	public void testSort() {
 		controller.executeCommand("sort");
 		Storage storage = new Storage("textbuddy.txt");
 		assertEquals("test sort", "a",storage.getContents().get(0));
 
 	}
 	@Test
-	public void testSearch(){
+	public void testSearch() {
 		controller.executeCommand("search b");
 		Storage storage = new Storage("textbuddy.txt");
 		assertEquals("test search", "4. b",storage.getSearchArray().get(0));
