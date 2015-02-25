6a36bfbbaeb1a2a6b4bb1ee2fb8939f4c58bdcb6
gableh <gable.heng@gmail.com>
2015-02-25 23:29:19 +0800

    refactor testcases

--- TextBuddyTest.java
+++ TextBuddyTest.java
@@ -1,13 +1,14 @@
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextBuddyTest {
 	String[] args = new String[0];
-	@Test
-	public void testController() {
-		Controller controller = new Controller(args);
-		assertEquals("test constructor","textbuddy.txt",controller.getFileName());
-		assertEquals("test is running",true,controller.getIsRunning());
+	private Controller controller;
+	@Before
+	public void init(){
+		controller = new Controller(args);
 		controller.executeCommand("clear");
 		controller.executeCommand("add g");
 		controller.executeCommand("add h");
@@ -15,18 +16,31 @@ public class TextBuddyTest {
 		controller.executeCommand("add b");
 		controller.executeCommand("add d");
 		controller.executeCommand("add c");
-		controller.executeCommand("sort");
-		controller.executeCommand("search b");
+	}
+	@Test
+	public void testController() {
+		assertEquals("test constructor","textbuddy.txt",controller.getFileName());
+		assertEquals("test is running",true,controller.getIsRunning());
 		
 	}
 	@Test
 	public void testStorage(){
 		Storage storage = new Storage("textbuddy.txt");
 		assertEquals("test storage constructor",true,storage.getIsSuccessful());
+	}
+	@Test
+	public void testSort(){
+		controller.executeCommand("sort");
+		Storage storage = new Storage("textbuddy.txt");
 		assertEquals("test sort", "a",storage.getContents().get(0));
-		assertEquals("test search", "2. b",storage.getSearchArray().get(0));
 
 	}
-	
+	@Test
+	public void testSearch(){
+		controller.executeCommand("search b");
+		Storage storage = new Storage("textbuddy.txt");
+		assertEquals("test search", "4. b",storage.getSearchArray().get(0));
+
+	}
 
 }
